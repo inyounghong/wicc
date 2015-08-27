87c151dd7e847e29840f10ceb0c612f5732910b6
Inyoung Hong <ih235@cornell.edu>
2015-08-27 11:38:44 -0400

    Opportunities title centering

--- opportunities.php
+++ opportunities.php
@@ -26,13 +26,13 @@
           <div class="col-sm-6">
             <!-- QAmp-->
             <a class="c" href="http://www.summerqamp.org/program"><img src="img/qamp.jpg"></a>
-            <h1><a href="http://www.summerqamp.org/program">Summer QAmp</a></h1>
+            <h3><a href="http://www.summerqamp.org/program">Summer QAmp</a></h3>
             <p>The goal of SummerQAmp is to introduce Quality Assurance (QA) as a potential career path to American youth (ages 18-24) who are unaware of the opportunity by allowing them to gain valuable work experience as summer interns at software companies.</p>
           </div>
           <div class="col-sm-6">
               <!--HackNY Fellows-->
-                  <a class="c" href="http://apply.hackny.org/"><img src="img/hackny.jpg"></a>
-                  <h1><a href="http://apply.hackny.org/">HackNY Fellows</a></h1>
+            <a class="c" href="http://apply.hackny.org/"><img src="img/hackny.jpg"></a>
+            <h3><a href="http://apply.hackny.org/">HackNY Fellows</a></h3>
             <p>For anyone with coding experience. The HackNY Fellows program, now entering its fifth year, is an intense program designed to introduce students to NYC's startup ecosystem by pairing the best technical minds with great New York startups.</p>
           </div>
         </div>
@@ -42,8 +42,8 @@
 
             <!--PennApps Fellows-->
             
-                  <a class="c" href="http://http://www.pennappsfellows.com/"><img src="img/pennapps.jpg"></a>
-                  <h1><a href="http://apply.hackny.org/">PennApps Fellows</a></h1>
+              <a class="c" href="http://http://www.pennappsfellows.com/"><img src="img/pennapps.jpg"></a>
+              <h3><a href="http://apply.hackny.org/">PennApps Fellows</a></h3>
             <p>PennApp Fellows is Philadelphia's premiere student-run fellowship program that involves a 10-week summer internship program that pairs talented students from across the world to top Philadelphia tech companies and startups.</p>
           </div>
           <div class="col-sm-6">        
@@ -51,7 +51,7 @@
             <!--Facebook U-->
             
                 <a class="c" href="https://www.google.com/url?q=https://www.facebook.com/careers/university/fbu/"><img src="img/facebook.jpg"></a>
-                <h1><a href="https://www.facebook.com/careers/university/fbu/">Facebook U</a></h1>
+                <h3><a href="https://www.facebook.com/careers/university/fbu/">Facebook U</a></h3>
             <p>For rising college Sophomores. FBU is a paid eight-week summer internship designed to provide iOS development experience to students who are historically under-represented in computer science.</p>
           </div>  
         </div>
@@ -60,13 +60,13 @@
           <div class="col-sm-6">
             <!--Google Engineering  Practicum-->
             <a class="c" href="https://www.google.com/about/careers/search#!t=jo&jid=3823002&"><img src="img/google.jpg"></a>
-            <h1><a href="https://www.google.com/about/careers/search?src=Online/TOPs/NA+Tech+University#!t=jo&jid=3823002">Google Engineering Practicum</a></h1>
+            <h3><a href="https://www.google.com/about/careers/search?src=Online/TOPs/NA+Tech+University#!t=jo&jid=3823002">Google Engineering Practicum</a></h3>
             <p>For rising college Sophomores and Juniors. The Engineering Practicum program is a 12-week developmental opportunity for first and second-year undergraduate students with a passion for technology -- especially students from historically underrepresented groups in the field.</p>
           </div>
           <div class="col-sm-6">
             <!--Microsoft Explore Program-->
             <a class="c" href="http://careers.microsoft.com/careers/en/us/university-programs.aspx#tab_explore-microsoft-program-overview"><img src="img/microsoft.jpg"></a>
-            <h1><a href="http://careers.microsoft.com/careers/en/us/university-programs.aspx#tab_explore-microsoft-program-overview">Microsoft Explore Program</a></h1>
+            <h3><a href="http://careers.microsoft.com/careers/en/us/university-programs.aspx#tab_explore-microsoft-program-overview">Microsoft Explore Program</a></h3>
             <p>For rising college Sophomores and Juniors. Explore Microsoft is a 12-week summer internship program that is specifically designed for college underclassmen (freshmen and sophomores) and offers a rotational experience aimed to enable you to gain experience in our different software engineering roles. </p>
           </div>
 
