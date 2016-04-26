d0b5d1245301494156eb6b3bd6b0cb48cc580e49
Inyoung Hong <ih235@cornell.edu>
2016-04-26 14:40:35 -0400

    calendar

--- calendar.php
+++ calendar.php
@@ -8,17 +8,22 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/main.css" />
     
-        <link rel="shortcut icon" href="img/wicc-logo.jpg" />
+    <link rel="shortcut icon" href="img/wicc-logo.jpg" />
     <link rel="shortcut icon" href="http://wicc.acm.org/img/wicc-logo.jpg">
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Raleway:400,600,500,700,900,800' rel='stylesheet' type='text/css'>
-    <script src="js/modernizr.js"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    
+    <!-- Calendar -->
+    <link rel='stylesheet' href='css/fullcalendar.min.css' />
+    <script src='js/jquery.min.js'></script>
+    <script src='js/moment.min.js'></script>
+    <script src='js/fullcalendar.js'></script>
+    <script src='js/gcal.js'></script>
+    <script src='js/jquery-ui.custom.min.js'></script>
+
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-  
-    <!-- jQuery library -->
-    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+
     <!-- FormatGoogleCalendar Javascript file -->
     <script src="./js/format-google-calendar.js"></script>
     <script>
@@ -26,6 +31,23 @@
         formatGoogleCalendar.init({
           upcomingTopN: 8
         });
+
+        $('#calendar').fullCalendar({
+            googleCalendarApiKey: 'AIzaSyDLE-WGNFn2JLI2yFyonCMVEz89v0-4ZWg',
+    
+            // US Holidays
+            events: 'cornell.edu_98tsboucmi53v2rlt7kdj43vkc@group.calendar.google.com',
+            
+            loading: function(bool) {
+              $('#loading').toggle(bool);
+            },
+            header: {
+              left: 'prev,next today',
+              center: 'title',
+              right: 'month,agendaWeek,agendaDay'
+            },
+            displayEventEnd: true
+        })
        })
       </script>
   </head>
@@ -41,15 +63,18 @@
       <br><br>
 
       <h2>Calendar</h2>
-
-      <div class="section" id="calendar">
+    </div>
+    
+    <div class="narrow">
+      <div id='calendar'></div>
+<!--       <div class="section" id="calendar2">
         <iframe src="https://calendar.google.com/calendar/embed?showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=cornell.edu_98tsboucmi53v2rlt7kdj43vkc%40group.calendar.google.com&amp;color=%232F6309&amp;ctz=America%2FNew_York" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe></div>
-      </div>
+      </div> -->
       
     </div>
+    
 
     <?php include 'includes/footer.html' ?>
-    <script src="js/jquery.js"></script>
 
   </body>
 </html>
