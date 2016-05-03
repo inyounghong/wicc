eb26b0bf07fadd46795ddbd2896cd30d60cec84f
Inyoung Hong <ih235@cornell.edu>
2016-05-03 11:47:42 -0400

    students

---
+++ students.php
@@ -0,0 +1,148 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Women in Computing at Cornell</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css" />
+    
+    <link rel="shortcut icon" href="img/icon.png" />
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,600,500,700,900,800' rel='stylesheet' type='text/css'>
+    <script src="js/modernizr.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+
+    <?php include 'includes/nav.html' ?>
+
+    <div class="container">
+    <div class="narrow">
+        <h2>Career Development</h2>
+
+        <div class="career_wrap">
+            <a class="career_item">Latex</a>
+            <a class="career_item">Technical Interviews</a>
+            <a class="career_item">Web Dev</a>
+        </div>
+        
+        <br><br>
+        <h2>Coding Resources</h2>
+
+        <div class="row">
+          <div class="col-sm-6">
+            <a class="c img-block" href="http://www.udemy.com/"><img src="img/udemy.jpg" width="90"></a>
+            <h3><a href="http://www.udemy.com/">Udemy</a></h3>
+            <p>"At Udemy, our mission is to help anyone learn anything. As part of our Corporate Social Responsibility Initiative, we want to help WICC members learn all they can for FREE. We have partnered with your WICC executive board at Cornell to bring you the most in-demand tools in programming languages, web, and mobile development. All of these tutorials come with in-depth videos, exercises, and real-life examples for all skill levels."</p>
+            <center>
+            <a href="https://www.udemy.com/courses/Development/Web-Development/?p=1&price=price-free">Web Development Courses</a><br>
+            <a href="https://www.udemy.com/courses/Development/Programming-Languages/?p=1&price=price-free">Programming Languages Courses</a><br>
+            <a href="https://www.udemy.com/courses/Development/Mobile-Apps/?p=1&price=price-free&">Mobile App development Courses</a>
+            </center>
+          </div>
+          <div class="col-sm-6">
+            <a class="c img-block" href="http://www.codecademy.com/"><img src="https://pbs.twimg.com/profile_images/499697083398381568/ih2vOpDt.png" height="150" width="150"></a>
+            <h3><a href="http://www.codecademy.com/">CodeCademy</a></h3>
+          <p>Learn to code interactively, for free! Codecademy hosts interactive courses on how to program in seven different programming languages: HTML, CSS, JavaScript, jQuery, Ruby PHP, and Python. </p>
+            </center>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-sm-6">
+            <a class="c img-block" href="http://www.udacity.com/"><img src="http://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Udacity_Logo.svg/2000px-Udacity_Logo.svg.png" width="100" ></a>
+            <h3><a href="http://www.udacity.com/">Udacity</a></h3>
+          <p>Udacity provides project-based online courses and credentials in programming that fit into your lifestyle. There are many free classes that range from teaching you the very basics of programming to more advanced courses that help you make sense of data. You can also earn a nanodegree to advance your career in programs such as full-stack development and iOS development.</p>
+            </center>
+          </div>
+
+        <div class="col-sm-6">
+            <a class="c img-block" href="https://www.coursera.org/"><img src="img/coursera.jpg" width="150"></a>
+            <h3><a href="https://www.coursera.org/">Coursera</a></h3>
+              <p>Education is changing, with great educators from around the world increasingly putting their amazing courses online. We believe we are in the early days of a revolution that will not only increase access to great education, but also transform the way people learn. SlideRule is our way of contributing to the movement. We're bridging the skills gap through affordable, high-quality online education. Our vision is to create a credible alternative to traditional education for the new economy.</p>
+            </center>
+          </div>
+        </div>
+
+
+        <div class="row">
+          <div class="col-sm-6">
+            <a class="c img-block" href="https://www.mysliderule.com/"><img src="https://d1qb2nb5cznatu.cloudfront.net/startups/i/270431-8e9451e1ad3372c78ccf005fa4b5f6c1-medium_jpg.jpg" width="150"></a>
+            <h3><a href="https://www.mysliderule.com/">SlideRule</a></h3>
+              <p>Coursera works with universities to offer masive open online coursers (MOOCs) for free. Take courses in artificial intelligence, data science, theoretical computer science, interface design, software engineering, computer systems, security, and many more! There is an extra option to earn verified certificate with official recognition for your achievements.</p>
+            </center>
+          </div>
+
+          <div class="col-sm-6">
+            <a class="c img-block" href="https://www.edx.org/"><img src="http://futureedu.edx.org/assets/images/edx.png" width="150"></a>
+            <h3><a href="https://www.edx.org/">edX</a></h3>
+              <p>At edX, we believe in the highest quality education, both online and in the classroom. EdX was created for students and institutions that seek to transform themselves through cutting-edge technologies, innovative pedagogy, and rigorous courses.</p>
+            </center>
+          </div>
+        </div>
+
+        <br><br>
+        <h2>Summer Opportunities</h2>
+
+        <div class="row">
+          <div class="col-sm-6">
+            <!-- QAmp-->
+            <a class="c" href="http://www.summerqamp.org/program"><img src="img/qamp.jpg"></a>
+            <h3><a href="http://www.summerqamp.org/program">Summer QAmp</a></h3>
+            <p>The goal of SummerQAmp is to introduce Quality Assurance (QA) as a potential career path to American youth (ages 18-24) who are unaware of the opportunity by allowing them to gain valuable work experience as summer interns at software companies.</p>
+          </div>
+          <div class="col-sm-6">
+              <!--HackNY Fellows-->
+            <a class="c" href="http://apply.hackny.org/"><img src="img/hackny.jpg"></a>
+            <h3><a href="http://apply.hackny.org/">HackNY Fellows</a></h3>
+            <p>For anyone with coding experience. The HackNY Fellows program, now entering its fifth year, is an intense program designed to introduce students to NYC's startup ecosystem by pairing the best technical minds with great New York startups.</p>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-sm-6">
+
+            <!--PennApps Fellows-->
+            
+              <a class="c" href="http://http://www.pennappsfellows.com/"><img src="img/pennapps.jpg"></a>
+              <h3><a href="http://apply.hackny.org/">PennApps Fellows</a></h3>
+            <p>PennApp Fellows is Philadelphia's premiere student-run fellowship program that involves a 10-week summer internship program that pairs talented students from across the world to top Philadelphia tech companies and startups.</p>
+          </div>
+          <div class="col-sm-6">        
+
+            <!--Facebook U-->
+            
+                <a class="c" href="https://www.google.com/url?q=https://www.facebook.com/careers/university/fbu/"><img src="img/facebook.jpg"></a>
+                <h3><a href="https://www.facebook.com/careers/university/fbu/">Facebook U</a></h3>
+            <p>For rising college Sophomores. FBU is a paid eight-week summer internship designed to provide iOS development experience to students who are historically under-represented in computer science.</p>
+          </div>  
+        </div>
+
+        <div class="row">
+          <div class="col-sm-6">
+            <!--Google Engineering  Practicum-->
+            <a class="c" href="https://www.google.com/about/careers/search#!t=jo&jid=3823002&"><img src="img/google.jpg"></a>
+            <h3><a href="https://www.google.com/about/careers/search?src=Online/TOPs/NA+Tech+University#!t=jo&jid=3823002">Google Engineering Practicum</a></h3>
+            <p>For rising college Sophomores and Juniors. The Engineering Practicum program is a 12-week developmental opportunity for first and second-year undergraduate students with a passion for technology -- especially students from historically underrepresented groups in the field.</p>
+          </div>
+          <div class="col-sm-6">
+            <!--Microsoft Explore Program-->
+            <a class="c" href="http://careers.microsoft.com/careers/en/us/university-programs.aspx#tab_explore-microsoft-program-overview"><img src="img/microsoft.jpg"></a>
+            <h3><a href="http://careers.microsoft.com/careers/en/us/university-programs.aspx#tab_explore-microsoft-program-overview">Microsoft Explore Program</a></h3>
+            <p>For rising college Sophomores and Juniors. Explore Microsoft is a 12-week summer internship program that is specifically designed for college underclassmen (freshmen and sophomores) and offers a rotational experience aimed to enable you to gain experience in our different software engineering roles. </p>
+          </div>
+      </div>
+        
+        
+        
+      </div>  
+    </div>
+        
+        
+	<?php include 'includes/footer.html' ?>
+  </body>
+</html>
