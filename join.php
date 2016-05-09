31ef85af7f7a7c08e87bf0771f2a84dff38989d4
Inyoung Hong <ih235@cornell.edu>
2016-05-09 16:41:28 -0400

    join page

--- join.php
+++ join.php
@@ -19,42 +19,57 @@
   <body>
     <?php include "includes/nav.html" ?>
 
-    <div class="container">
-      <div class="middle">
-        
-          <div class="row">
-            <div class="col-sm-7">
-              <h1>Join WICC</h1>
+      <div class="container" id="joinus">
+        <h2>Join Us</h2>
 
-              Want to get involved in WICC? Sign up for our listserv to receive notifications about all of our events: <br>
-              <br>Send a <u>blank</u> e-mail to <a href="mailto:wicc-l-request@cornell.edu">wicc-l-request@cornell.edu</a> with the subject <b>join</b> .
+        <div class="row wrap narrow">
+          <div class="col-sm-6 c">
 
-              
+            <h3>Students</h3>
+            <form method="post" action="php/subscribe.php">
+              <input type="text" class="listserve" name="netid" id="netid" placeholder="Cornell NetID">
+              <a id="submit" class="button teal">Join Our Listserve</a>
+              <div id="error"></div>
+            </form>
+          </div>
 
+          <div class="col-sm-6 c">
+            <h3>Corporate</h3>
+            <p>Contact us at <br><a href="mailto:wicc@cornell.edu">wicc@cornell.edu</a></p>
+            <a href="https://docs.google.com/a/cornell.edu/forms/d/1znGOx1z6wn3O-ZrnrIKAOTvnyaJ5qRvlPoC3QSxazp0/viewform" class="button teal">Host an event</a>
+          </div>
+        </div>
+        <br>
 
-            </div>
-            <div class="col-sm-1"></div>
-            <div class="col-sm-4">
-              <p><img src="img/social-media/facebook-square.jpg" class="fb" width="25">
-              <a href="https://www.facebook.com/CornellWomenInComputing">Women in Computing at Cornell</a><br>
-              Like us on Facebook!</p>
+        <div class="row wrap narrow">
+          <div class="col-sm-6 c">
+            <h3>Alumni</h3>
+            <p>Join our alumni network!</p>
+            <a href="#" class="button teal">Coming Soon</a>
+          </div>
 
-              <p><img src="img/social-media/facebook-square.jpg" class="fb" width="25">
-              <a href="https://www.facebook.com/groups/467594836608391/">WICC Members</a>
-              <br>
-              We invite you to join our Facebook group, where we're stimulating a dialogue about topics relating to women in technology.</p><br>
-            
-              
+          <div class="col-sm-6 c">
+            <h3>Mentorship</h3>
+            <p>Participate in our mentorship program as a mentor or a mentee!</p>
+            <a href="http://localhost/wicc/mentorship.php" class="button teal">Learn More</a>
+          </div>
+        </div>
+
+        <br>
+
+        <div class="c">
+            <h3>Connect with WICC</h3>
+
+            <div class="media-buttons">
+              <a href="https://www.facebook.com/CornellWomenInComputing"><img src="img/social_media/facebook_square.png"></a>
+              <a href="https://www.linkedin.com/groups/8480565"><img src="img/social_media/linkedin_square.png"></a>
+              <a href="https://www.instagram.com/wiccornell/"><img src="img/social_media/instagram_square.png"></a>
             </div>
           </div>
         </div>
-        
-    
-      
-      </div>
-    </div>
+        <br><br>
 
-    <br><br><br><br><br><br><br><br>
+      </div>
 
     <?php include 'includes/footer.html' ?>
 
